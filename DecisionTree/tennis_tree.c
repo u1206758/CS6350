ab9dca26fc3ebb862205adf213e5b8cb9dd98261
Adam Blakeslee <adam.blakeslee@utah.edu>
2024-09-20 12:40:11 -0600

    decision tree implemented in tennis_tree.c. Need to add error calculations, then duplicate to use on car datasets:

--- DecisionTree/tennis_tree.c
+++ DecisionTree/tennis_tree.c
@@ -3,32 +3,105 @@
 #include <string.h>
 #include <stdbool.h>
 
-/*
-Import tree
-Import data (except labels)
-    for each dataset instance
-        start at branch 0
-        if current branch has a label
-            assign the label to the dataset
-        else
-            move to the branch where currentbranch.leaf[i].value == instance value for currentbranch.attribute
-*/
+#define NUM_ATTRIBUTES 4
+#define MAX_VAL 3 
 
-int countData(void);
-int importData(int data[][NUM_ATTRIBUTES+1], int numInstances);
+#define DATA_FILE "tennis.csv"
+#define TREE_FILE "tennis_tree.csv"
+
+int countEntries(char fileName[]);
+int importData(char fileName[], int data[][NUM_ATTRIBUTES+1], int numInstances, int numAttributes);
+int importTree(char fileName[], int tree[][8], int numInstances, int numAttributes);
+int valueToInt(char* value);
+
+typedef struct
+{
+    int id;
+    int attribute;
+    int value;
+    int label;
+    int parent;
+    int leaf[MAX_VAL];
+}Branch;
 
 int main()
 {
+    //Import data from CSV
+    int numInstances = countEntries(DATA_FILE);
+    if (numInstances == -1)
+    {
+        return 1;
+    }
+    int data[numInstances][NUM_ATTRIBUTES+1];
+    importData(DATA_FILE, data, numInstances, NUM_ATTRIBUTES+1);
+    int dataLabels[numInstances];
+    int myLabels[numInstances];
+    //Set up label arrays for error calculations
+    for (int i = 0; i < numInstances; i++)
+    {
+        dataLabels[i] = data[i][NUM_ATTRIBUTES];
+        myLabels[i] = -3;
+    }
+
+    //Import tree from CSV
+    int numBranches = countEntries(TREE_FILE);
+    int tempTree[numBranches][8];
+    Branch tree[numBranches];
+    importTree(TREE_FILE, tempTree, numBranches, 8);
+    for (int i = 0; i < numBranches; i++)
+    {
+        tree[i].id = tempTree[i][0];
+        tree[i].attribute = tempTree[i][1];
+        tree[i].value = tempTree[i][2];
+        tree[i].label = tempTree[i][3];
+        tree[i].parent = tempTree[i][4];
+        for (int j = 0; j < MAX_VAL; j++)
+        {
+            tree[i].leaf[j] = tempTree[i][5+j];
+        }
+    }
+
+    //Put data through decision tree
+    int branchIndex = 0;
+    int instanceIndex = 0;
+    //For each instance in dataset
+    while (instanceIndex < numInstances)
+    {   
+        //If the current branch in the tree has a label, assign that label to that instance
+        if (tree[branchIndex].label > -1)
+        {
+            myLabels[instanceIndex] = tree[branchIndex].label;
+            branchIndex = 0;
+            instanceIndex++;
+        }
+        //If the current branch does not have a label, move to the leaf whose value on the split attribute matches the instance
+        else
+        {
+            for (int j = 0; j < MAX_VAL; j++)
+            {
+                if (data[instanceIndex][tree[branchIndex].attribute] == tree[tree[branchIndex].leaf[j]].value)
+                {
+                    branchIndex = tree[branchIndex].leaf[j];
+                    break;
+                }
+            }
+            
+        }
+    }
+
+    for (int i = 0; i < numInstances; i++)
+    {
+        printf("%d  --  %d\n", dataLabels[i], myLabels[i]);
+    }
 
 }
 
-
-int countData(void)
+int countEntries(char fileName[])
 {
      int count = 0;
 
     //Open input file
-    FILE *inputFile = fopen("tennis.csv", "r");
+    FILE *inputFile = fopen(fileName, "r");
     if (inputFile == NULL)
     {
         printf("Error opening file");
@@ -54,9 +127,9 @@ int countData(void)
     return count;
 }
 
-int importData(int data[][NUM_ATTRIBUTES+1], int numInstances)
+int importData(char fileName[], int data[][NUM_ATTRIBUTES+1], int numInstances, int numAttributes)
 {
-    FILE *inputFile = fopen("tennis.csv", "r");
+    FILE *inputFile = fopen(fileName, "r");
     if (inputFile == NULL)
     {
         printf("Error opening file");
@@ -79,7 +152,7 @@ int importData(int data[][NUM_ATTRIBUTES+1], int numInstances)
             else
             {
                 token = strtok(row, ",");
-                for (int j = 0; j < NUM_ATTRIBUTES+1; j++)
+                for (int j = 0; j < numAttributes; j++)
                 {
                     data[i][j] = valueToInt(token);
                     token = strtok(NULL, ",\r\n");
@@ -89,4 +162,90 @@ int importData(int data[][NUM_ATTRIBUTES+1], int numInstances)
     }
     fclose(inputFile);
     return 0;
+}
+
+int importTree(char fileName[], int tree[][8], int numInstances, int numAttributes)
+{
+    FILE *inputFile = fopen(fileName, "r");
+    if (inputFile == NULL)
+    {
+        printf("Error opening file");
+        return -1;
+    }
+
+    char row[100];
+    char *token;
+
+    //Parse input CSV into data instance struct array
+    while (feof(inputFile) != true)
+    {
+        for (int i = 0; i < numInstances; i++)
+        {
+            if (fgets(row, 100, inputFile) == NULL)
+            {
+                fclose(inputFile);
+                return 0;
+            }
+            else
+            {
+                token = strtok(row, ",");
+                for (int j = 0; j < numAttributes; j++)
+                {
+                    tree[i][j] = valueToInt(token);
+                    token = strtok(NULL, ",\r\n");
+                }
+            }
+        }
+    }
+    fclose(inputFile);
+    return 0;
+}
+
+//Convert value strings from input dataset to integers
+int valueToInt(char* value)
+{
+    //branchIndex, attribute, value, label, parent, MAX_VAL # of leaves
+    int dec;
+    if (strlen(value) < 3 && (value[0] == '-' || value[0] <= '9'))
+    {
+        if ((char )value[0] == '-')
+        {
+            dec = -1 * ((char) value[1] - 48);
+        }
+        else
+        {
+            dec = (char) value[0] - 48;
+        }
+        return dec;
+    }
+
+    /*
+        data[inst][0] - outlook
+            sunny - 0
+            overcast - 1
+            rainy - 2
+        data[]inst[1] - temperature
+            hot - 0
+            medium - 1
+            cool - 2
+        data[inst][2] - humidity
+            high - 0
+            normal - 1
+            low - 2
+        data[inst][3] - wind
+            strong - 0
+            weak - 1
+        data[inst][4] - play
+            no - 0
+            yes - 1
+    */   
+
+    if (!strcmp(value, "sunny") || !strcmp(value, "hot") || !strcmp(value, "high") || !strcmp(value, "strong") || !strcmp(value, "no"))
+        return 0;
+    else if (!strcmp(value, "overcast") || !strcmp(value, "medium") || !strcmp(value, "normal") || !strcmp(value, "weak") || !strcmp(value, "yes"))
+        return 1;
+    else if (!strcmp(value, "rainy") || !strcmp(value, "cool") || !strcmp(value, "low"))
+        return 2;
+    else
+        return -1;
 }
